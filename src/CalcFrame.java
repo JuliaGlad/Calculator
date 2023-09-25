8d5c3def507a4427a89d1ab57860844aafde6763
y.gladkikh <y.gladkikh@tinkoff.ru>
2023-09-25 11:49:49 +0300

    Created Constants file

--- src/CalcFrame.java
+++ src/CalcFrame.java
@@ -20,16 +20,6 @@ public class CalcFrame extends JFrame implements ActionListener {
 
     String fieldText = "0";
 
-    public static final String SUM_OPERATION = "+";
-    public static final String MINUS_OPERATION = "-";
-    public static final String MULTIPLY_OPERATION = "*";
-    public static final String DIVIDE_OPERATION = "/";
-    public static final String EQUALS = "=";
-    public static final String DELETE_ALL = "C";
-    public static final String DELETE_PREVIOUS = "Delete";
-    public static final String ADD_MINUS = " -";
-    public static final String ADD_POINT = ".";
-
     public void init() {
 
         JFrame frameNew = new JFrame("Calculator");
@@ -65,63 +55,63 @@ public class CalcFrame extends JFrame implements ActionListener {
         sum.setBounds(10, 100, 60, 40);
         sum.setFont(new Font("Arial", Font.BOLD, 18));
         sum.addActionListener(this);
-        sum.setActionCommand(SUM_OPERATION);
+        sum.setActionCommand(Constants.SUM_OPERATION);
         panel.add(sum);
 
         JButton minus = new JButton("-");
         minus.setBounds(10, 100, 60, 40);
         minus.setFont(new Font("Arial", Font.BOLD, 18));
         minus.addActionListener(this);
-        minus.setActionCommand(MINUS_OPERATION);
+        minus.setActionCommand(Constants.MINUS_OPERATION);
         panel.add(minus);
 
         JButton mult = new JButton("*");
         mult.setBounds(10, 100, 60, 40);
         mult.setFont(new Font("Arial", Font.BOLD, 19));
         mult.addActionListener(this);
-        mult.setActionCommand(MULTIPLY_OPERATION);
+        mult.setActionCommand(Constants.MULTIPLY_OPERATION);
         panel.add(mult);
 
         JButton div = new JButton("/");
         div.setBounds(10, 100, 60, 40);
         div.setFont(new Font("Arial", Font.BOLD, 20));
         div.addActionListener(this);
-        div.setActionCommand(DIVIDE_OPERATION);
+        div.setActionCommand(Constants.DIVIDE_OPERATION);
         panel.add(div);
 
         JButton eq = new JButton("=");
         eq.setBounds(10, 100, 60, 40);
         eq.setFont(new Font("Arial", Font.BOLD, 17));
         eq.addActionListener(this);
-        eq.setActionCommand(EQUALS);
+        eq.setActionCommand(Constants.EQUALS);
         panel.add(eq);
 
         JButton act = new JButton("C");
         act.setBounds(10, 100, 60, 40);
         act.setFont(new Font("Arial", Font.BOLD, 17));
         act.addActionListener(this);
-        act.setActionCommand(DELETE_ALL);
+        act.setActionCommand(Constants.DELETE_ALL);
         panel.add(act);
 
         JButton dot = new JButton(".");
         dot.setBounds(10, 100, 60, 40);
         dot.setFont(new Font("Arial", Font.BOLD, 17));
         dot.addActionListener(this);
-        dot.setActionCommand(ADD_POINT);
+        dot.setActionCommand(Constants.ADD_POINT);
         panel.add(dot);
 
         JButton del = new JButton("Delete");
         del.setBounds(10, 100, 60, 40);
         del.setFont(new Font("Arial", Font.BOLD, 17));
         del.addActionListener(this);
-        del.setActionCommand(DELETE_PREVIOUS);
+        del.setActionCommand(Constants.DELETE_PREVIOUS);
         panel.add(del);
 
         JButton minusNum = new JButton("+/-");
         minusNum.setBounds(10, 100, 60, 40);
         minusNum.setFont(new Font("Arial", Font.BOLD, 17));
         minusNum.addActionListener(this);
-        minusNum.setActionCommand(ADD_MINUS);
+        minusNum.setActionCommand(Constants.ADD_MINUS);
         panel.add(minusNum);
     }
 
@@ -129,50 +119,48 @@ public class CalcFrame extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         String action = e.getActionCommand();
         switch (action) {
-            case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "." :
+            case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".":
                 fieldText += action;
                 break;
-            case SUM_OPERATION, MINUS_OPERATION, MULTIPLY_OPERATION, DIVIDE_OPERATION:
+            case Constants.SUM_OPERATION, Constants.MINUS_OPERATION, Constants.MULTIPLY_OPERATION, Constants.DIVIDE_OPERATION:
                 firstNum = Double.parseDouble(textField.getText());
                 fieldText += action;
                 currentAction = action;
                 break;
-            case EQUALS:
+            case Constants.EQUALS:
                 int index = fieldText.indexOf(currentAction);
                 secondNum = Double.parseDouble(fieldText.substring(index + 1));
                 switch (currentAction) {
-                    case SUM_OPERATION:
+                    case Constants.SUM_OPERATION:
                         fieldText = String.valueOf(firstNum + secondNum);
                         break;
-                    case MINUS_OPERATION:
+                    case Constants.MINUS_OPERATION:
                         fieldText = String.valueOf(firstNum - secondNum);
                         break;
-                    case MULTIPLY_OPERATION:
+                    case Constants.MULTIPLY_OPERATION:
                         fieldText = String.valueOf(firstNum * secondNum);
                         break;
-                    case DIVIDE_OPERATION:
+                    case Constants.DIVIDE_OPERATION:
                         fieldText = String.valueOf(firstNum / secondNum);
                         break;
                 }
         }
-        if (action.equals(DELETE_ALL)) {
+        if (action.equals(Constants.DELETE_ALL)) {
             fieldText = String.valueOf(0);
         }
-        if (action.equals(DELETE_PREVIOUS)) {
+        if (action.equals(Constants.DELETE_PREVIOUS)) {
             if (fieldText != null) {
                 fieldText = fieldText.substring(0, fieldText.length() - 1);
             }
         }
-        if (action.equals(ADD_MINUS)){
+        if (action.equals(Constants.ADD_MINUS)) {
             fieldText += action;
-            switch (action){
-                case SUM_OPERATION, MINUS_OPERATION, MULTIPLY_OPERATION, DIVIDE_OPERATION -> fieldText += "-";
-            }
+            fieldText += "-";
         }
         if (fieldText.startsWith("0")) {
             fieldText = fieldText.substring(1);
         }
-        if (fieldText.endsWith(".0")){
+        if (fieldText.endsWith(".0")) {
             fieldText = fieldText.substring(0, fieldText.length() - 2);
         }
         textField.setText(fieldText);
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
-import javax.swing.*;
 
 public class Main   {
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                CalcFrame.setDefaultLookAndFeelDecorated(true);
-                CalcFrame frame = new CalcFrame();
-                frame.setLocationRelativeTo(null);
-            }
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            CalcFrame.setDefaultLookAndFeelDecorated(true);
+            CalcFrame frame = new CalcFrame();
+            frame.setLocationRelativeTo(null);
         });
     }
 }
